48d55da1ff8f2eed6d92f26ec50d4e27055d65d1
almuhajirin313 <111078736+almuhajirin313@users.noreply.github.com>
2026-09-20 23:57:34 +0700

    Delete assets directory

--- assets/js/api.js
+++
@@ -1,8 +0,0 @@
-async function apiGet(action){
- const u=new URL(window.APP_CONFIG.API_URL);u.searchParams.set('action',action);
- const r=await fetch(u);return r.json();
-}
-async function apiPost(action,data={},token=localStorage.getItem('absen_token')){
- const r=await fetch(window.APP_CONFIG.API_URL,{method:'POST',headers:{'Content-Type':'text/plain;charset=utf-8'},body:JSON.stringify({action,...data,token})});
- return r.json();
-}
