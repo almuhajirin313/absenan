fe96ad9fc65fb5bd965d74357c1ebea129b604a3
almuhajirin313 <111078736+almuhajirin313@users.noreply.github.com>
2026-09-21 00:11:50 +0700

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG={
-  API_URL:'PASTE_URL_WEB_APP_GAS_DI_SINI',
+  API_URL:'https://script.google.com/macros/s/AKfycby96ZpuurFtYYmfGfVOrUGQ5m62RTVpfDB7iJg0jxATpenq8q-E2ELFfZz6XAkH-UcQ/exec',
   MAP_TILE_URL:'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
   MAP_ATTRIBUTION:'&copy; OpenStreetMap contributors'
 };
